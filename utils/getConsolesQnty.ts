82a1bee92552e921012a4bf402d06d453bd17093
Petr Tcoi <petrtcoi@outlook.com>
2022-06-07 15:12:21 +0300

    fix calc consoles

--- utils/getConsolesQnty.ts
+++ utils/getConsolesQnty.ts
@@ -8,11 +8,68 @@ type getConsolesQnty = {
 
 const getConsolesQnty = ({ model, sectionQnty }: getConsolesQnty): number => {
 
-    const qnty = sectionQnty < 21 ? 2 :
-        sectionQnty < 31 ? 3 :
-            sectionQnty < 41 ? 4 :
-                sectionQnty < 51 ? 5 :
-                    0
+    let qnty = 0
+
+    switch (true) {
+
+
+        case +model.width <= 145 && +model.height <= 1000:
+            qnty = sectionQnty <= 28 ? 2 :
+                sectionQnty <= 42 ? 3 :
+                    sectionQnty <= 52 ? 4 :
+                        sectionQnty <= 60 ? 5 :
+                            6
+            break
+
+
+
+        case +model.width <= 145 && +model.height <= 2200:
+            qnty = sectionQnty <= 16 ? 2 :
+                sectionQnty <= 28 ? 3 :
+                    sectionQnty <= 38 ? 4 :
+                        sectionQnty <= 48 ? 5 :
+                            sectionQnty <= 60 ? 6 :
+                                7
+            break
+
+        case +model.width <= 145 && +model.height <= 3000:
+            qnty = sectionQnty <= 16 ? 2 :
+                sectionQnty <= 24 ? 3 :
+                    sectionQnty <= 30 ? 4 :
+                        5
+            break
+
+        case +model.width > 145 && +model.height <= 550:
+            qnty = sectionQnty <= 12 ? 2 :
+                sectionQnty <= 30 ? 3 :
+                    sectionQnty <= 40 ? 4 :
+                        sectionQnty <= 50 ? 5 :
+                            sectionQnty <= 60 ? 6 :
+                                7
+            break
+
+        case +model.width > 145 && +model.height <= 1500:
+            qnty = sectionQnty <= 16 ? 2 :
+                sectionQnty <= 26 ? 3 :
+                    sectionQnty <= 36 ? 4 :
+                        sectionQnty <= 46 ? 5 :
+                            sectionQnty <= 56 ? 6 :
+                                7
+            break
+
+        case +model.width > 145 && +model.height <= 3000:
+            qnty = sectionQnty <= 10 ? 2 :
+                sectionQnty <= 18 ? 3 :
+                    sectionQnty <= 24 ? 4 :
+                        sectionQnty <= 30 ? 5 :
+                            6
+            break
+
+        default:
+            qnty = 4
+
+    }
+
 
 
     return qnty

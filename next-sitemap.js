fb19edba6de41102626dfa14b9117c32dcecdc89
Petr Tcoi <petrtcoi@outlook.com>
2022-09-20 00:44:33 +0300

    fix robots clean-param

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://arboniashop.ru',
-    generateRobotsTxt: true, // (optional)
+    // generateRobotsTxt: true, // (optional)
     // ...other options
   }

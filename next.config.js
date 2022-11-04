6aea76e0358a524c518eed2bba49800cedf6b8c4
Petr Tcoi <petrtcoi@outlook.com>
2022-11-04 12:47:26 +0300

    try cache-control

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [{
+      source: "/(.*?)",
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'max-age=31536000',
+        }
+      ]
+    }]
+  }
 }

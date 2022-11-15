8edddf3e021931b3787c8f5b11047213dbc5c460
Petr Tcoi <petrtcoi@outlook.com>
2022-11-15 17:31:14 +0300

    1

--- components/layout/headerScripts.tsx
+++ components/layout/headerScripts.tsx
@@ -14,17 +14,17 @@ const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
             </Head>
             <YMInitializer accounts={ [89126477] } options={ { defer: true } } />
             <Script
-                strategy="worker"
+                strategy='afterInteractive'
                 src="//code-ya.jivosite.com/widget/y6IaQXr7wR"
             />
             <Script
-                strategy='worker'
+                strategy='afterInteractive'
                 id={ 'google_analytics_1' }
                 src="https://www.googletagmanager.com/gtag/js?id=UA-59967703-20"
             />
             <Script
                 defer
-                strategy="worker"
+                strategy="afterInteractive"
                 id={ 'google_analytics_2' }
                 dangerouslySetInnerHTML={ {
                     __html: `

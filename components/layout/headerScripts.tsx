33ee42d94f055cb47403ff8aa01644d5ca0a3c5a
Petr Tcoi <petrtcoi@outlook.com>
2022-12-08 16:22:44 +0300

    fix pixel VK

--- components/layout/headerScripts.tsx
+++ components/layout/headerScripts.tsx
@@ -43,7 +43,7 @@ const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
                 id="vk_pixel"
                 dangerouslySetInnerHTML={ {
                     __html: `
-                    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1622038-fv2ia"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1622038-fv2ia" style="position:fixed; left:-999px;" alt=""/></noscript>
+                    function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1622038-fv2ia"),VK.Retargeting.Hit()},document.head.appendChild(t)}();
                     `
                 } }
             />

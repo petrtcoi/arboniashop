34ebe5816d59dc4cd9283eb7abb3106ced6f1172
Petr Tcoi <petrtcoi@outlook.com>
2022-12-24 19:50:32 +0300

    GA4

--- components/layout/headerScripts.tsx
+++ components/layout/headerScripts.tsx
@@ -11,38 +11,31 @@ const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
         <>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <script
+                    defer
+                    id={ 'google_analytics_2' }
+                    dangerouslySetInnerHTML={ {
+                        __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KB7LCQ7');
+                    `,
+                    } }
+                />
             </Head>
             <YMInitializer accounts={ [89126477] } options={ { defer: true, webvisor: true, ecommerce: "dataLayer", trackLinks: true, accurateTrackBounce: true, clickmap: true, } } />
-            <Script
-                id='jivo'
-                strategy='afterInteractive'
-                src="//code-ya.jivosite.com/widget/y6IaQXr7wR"
-            />
-            <Script
-                strategy='afterInteractive'
-                id={ 'google_analytics_1' }
-                src="https://www.googletagmanager.com/gtag/js?id=UA-59967703-20"
-            />
-            <Script
-                defer
-                strategy="afterInteractive"
-                id={ 'google_analytics_2' }
-                dangerouslySetInnerHTML={ {
-                    __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
+            <noscript dangerouslySetInnerHTML={ {
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB7LCQ7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`} }></noscript>
 
-                    gtag('config', 'UA-59967703-20');
-                    `,
-                } }
-            />
             <Script
                 async
                 strategy="afterInteractive"
                 id="vk_pixel"
             >
-                {/* { `function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1622038-fv2ia"),VK.Retargeting.Hit()},document.head.appendChild(t)}();` } */}
+                {/* { `function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1622038-fv2ia"),VK.Retargeting.Hit()},document.head.appendChild(t)}();` } */ }
             </Script>
         </>
     )

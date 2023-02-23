65eff74248a88b6867d2f3d175e474ad4f8d4f17
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-02-23 12:58:40 +0300

    useLayoutEffect to update euroRate

--- api_utils/useGetCurrency.ts
+++ api_utils/useGetCurrency.ts
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { DISCOUNT, GET_RATE_URL } from '../variables/currency'
+import { useState, useEffect, useLayoutEffect } from 'react';
+import axios from 'axios';
+import { DISCOUNT, GET_RATE_URL } from '../variables/currency';
 
 
 
-const useGetCurrency = (): { rateEuro: number } => {
+const useGetCurrency = (): { rateEuro: number; } => {
 
-    const [rateEuro, setRateEuro] = useState<number>(0)
-    useEffect(() => {
+    const [rateEuro, setRateEuro] = useState<number>(0);
+    useLayoutEffect(() => {
         const getRate = async () => {
             try {
-                const data = await axios.get(`${GET_RATE_URL}/ARBONIASHOP` )
-                if (data.status !== 200 || data.data.rate <= 0) throw new Error()
-                setRateEuro(data.data.rate * DISCOUNT)
-            } catch (err){
-                setRateEuro(0)
+                console.log('FETCH DATA');
+                const data = await axios.get(`${GET_RATE_URL}/ARBONIASHOP`);
+                if (data.status !== 200 || data.data.rate <= 0) throw new Error();
+                setRateEuro(data.data.rate * DISCOUNT);
+            } catch (err) {
+                setRateEuro(0);
             }
-        }
-        getRate()
-    }, [])
+        };
+        getRate();
+    }, []);
 
-    return { rateEuro }
-}
+    return { rateEuro };
+};
 
-export default useGetCurrency
+export default useGetCurrency;

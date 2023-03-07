713f54f036c9c57fd3ff0dff87c886aaf9d94516
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-03-07 22:33:27 +0300

    set new phone numbers

--- variables/global.ts
+++ variables/global.ts
@@ -1,8 +1,8 @@
-const global: { [key: string]: any } = {
-    phone812String: '(812) 648-81-27',
-    phone812: '+78126488127',
-    phone495String: '(495)  008-81-27',
-    phone495: '+74950088127',
+const global: { [key: string]: any; } = {
+    phone812String: '(812) 409-38-55',
+    phone812: '+78124093855',
+    phone495String: '(495)  204-29-55',
+    phone495: '+74952042955',
     whatsAppString: '+1 (218) 516-15-29',
     whatsApp: '12185161529',
     pages: [
@@ -21,6 +21,6 @@ const global: { [key: string]: any } = {
     sectionsDefaultMin: 4,
     sectionsDefaultMax: 64,
     pagePadding: { paddingTop: "30px", paddingBottom: "40px", paddingX: { xs: "20px", md: "40px" } }
-}
+};
 
-export default global
+export default global;

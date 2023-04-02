3dc4801517b47393417bdc91070635752218fe79
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-04-03 00:43:24 +0300

    add headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+
+const ContentSecurityPolicy = `
+  script-src 'self' https://mc.yandex.ru https://yastatic.net https://ucalc.pro http://localhost:3000;
+`
+
+
 module.exports = {
   reactStrictMode: true,
   i18n: {
@@ -14,7 +20,39 @@ module.exports = {
           {
             key: 'cache-control',
             value: 'public, max-age=31536000, immutable',
-          }
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          // }
         ]
       }
     ]

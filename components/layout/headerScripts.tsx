b66e9d1d4bfd57bc15bd109c057d5b9b706feff8
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-03-04 11:59:04 +0300

    add vk Pixel

--- components/layout/headerScripts.tsx
+++ components/layout/headerScripts.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head"
-import Script from "next/script"
-import React from "react"
-import { YMInitializer } from "react-yandex-metrika"
+import Head from "next/head";
+import Script from "next/script";
+import React from "react";
+import { YMInitializer } from "react-yandex-metrika";
 
-type HeaderScriptsProps = {}
+type HeaderScriptsProps = {};
 
 const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
   return (
@@ -12,15 +12,15 @@ const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <YMInitializer
-        accounts={ [ 89126477 ] }
-        options={ {
+        accounts={[89126477]}
+        options={{
           defer: true,
           webvisor: true,
           ecommerce: "dataLayer",
           trackLinks: true,
           accurateTrackBounce: true,
           clickmap: true,
-        } }
+        }}
       />
 
       <Script
@@ -28,8 +28,13 @@ const HeaderScripts: React.FC<HeaderScriptsProps> = () => {
         strategy="afterInteractive"
         src="//code-ya.jivosite.com/widget/y6IaQXr7wR"
       />
-    </>
-  )
-}
 
-export default HeaderScripts
+      <Script id="vk-pixel">
+        {`!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-890788-ib6bm"), VK.Retargeting.Hit()},document.head.appendChild(t)}();`}
+      </Script>
+
+    </>
+  );
+};
+
+export default HeaderScripts;

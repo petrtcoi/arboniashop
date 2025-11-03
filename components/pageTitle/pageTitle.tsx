aa4de0660284a249c84a6a6a4178ae3bfffd83b0
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-11-03 13:40:21 +0100

    fix

--- components/pageTitle/pageTitle.tsx
+++ components/pageTitle/pageTitle.tsx
@@ -31,7 +31,7 @@ const PageTitle: React.FC<PageTitleProps> = ({ header, subheader, breadcrumbs })
 
 	return (
 		<>
-			{!breadcrumbs || breadcrumbs.length === 0 ? (
+			{!!breadcrumbs && breadcrumbs.length > 0 ? (
 				<Script
 					id='jsonld'
 					type='application/ld+json'

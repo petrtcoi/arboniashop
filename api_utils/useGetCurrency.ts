3df87491fb769fa787bd22a5ae235e84fb2dcd0c
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2026-03-30 11:47:45 +0200

    refactor: Update currency fetching logic in useGetCurrency hook, adjusting default rate and cleaning up commented code for clarity

--- api_utils/useGetCurrency.ts
+++ api_utils/useGetCurrency.ts
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { DISCOUNT, GET_RATE_URL } from "../variables/currency";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { DISCOUNT, GET_RATE_URL } from '../variables/currency'
 
-const DEFAULT_CURRENCY_RATE = 102;
+const DEFAULT_CURRENCY_RATE = 102.85
 
-const DISCOUNTED_DEF_PRICE = DEFAULT_CURRENCY_RATE * DISCOUNT;
+const DISCOUNTED_DEF_PRICE = DEFAULT_CURRENCY_RATE * DISCOUNT
 
 const useGetCurrency = (): { rateEuro: number; isFetching: boolean } => {
-  const [rateEuro, setRateEuro] = useState<number>(DISCOUNTED_DEF_PRICE);
+	const [rateEuro, setRateEuro] = useState<number>(DISCOUNTED_DEF_PRICE)
 
-  const [isFetching, setIsFetching] = useState<boolean>(true);
+	const [isFetching, setIsFetching] = useState<boolean>(true)
 
-  useEffect(() => {
-    const getRate = async () => {
-      try {
-        const data = await axios.get(`${GET_RATE_URL}/ARBONIASHOP`);
-        if (data.status !== 200 || data.data.rate <= 0) throw new Error();
-        setRateEuro(data.data.rate * DISCOUNT);
-        setIsFetching(false);
-      } catch (err) {
-        setRateEuro(DISCOUNTED_DEF_PRICE);
-      }
-    };
-    getRate();
-  }, []);
+	useEffect(() => {
+		const getRate = async () => {
+			try {
+				// const data = await axios.get(`${GET_RATE_URL}/ARBONIASHOP`)
+				// if (data.status !== 200 || data.data.rate <= 0) throw new Error()
+				setRateEuro(DEFAULT_CURRENCY_RATE * DISCOUNT)
+				setIsFetching(false)
+			} catch (err) {
+				setRateEuro(DISCOUNTED_DEF_PRICE)
+			}
+		}
+		getRate()
+	}, [])
 
-  return { rateEuro, isFetching };
-};
+	return { rateEuro, isFetching }
+}
 
-export default useGetCurrency;
+export default useGetCurrency

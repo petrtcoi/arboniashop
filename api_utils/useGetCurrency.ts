1dedebcd7395eae15f103768c777891172051683
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2026-04-10 18:27:06 +0200

    chore: update default currency rate and apply consistent code formatting

--- api_utils/useGetCurrency.ts
+++ api_utils/useGetCurrency.ts
@@ -1,31 +1,31 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { DISCOUNT, GET_RATE_URL } from '../variables/currency'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { DISCOUNT, GET_RATE_URL } from '../variables/currency';
 
-const DEFAULT_CURRENCY_RATE = 102.85
+const DEFAULT_CURRENCY_RATE = 104.54;
 
-const DISCOUNTED_DEF_PRICE = DEFAULT_CURRENCY_RATE * DISCOUNT
+const DISCOUNTED_DEF_PRICE = DEFAULT_CURRENCY_RATE * DISCOUNT;
 
-const useGetCurrency = (): { rateEuro: number; isFetching: boolean } => {
-	const [rateEuro, setRateEuro] = useState<number>(DISCOUNTED_DEF_PRICE)
+const useGetCurrency = (): { rateEuro: number; isFetching: boolean; } => {
+	const [rateEuro, setRateEuro] = useState<number>(DISCOUNTED_DEF_PRICE);
 
-	const [isFetching, setIsFetching] = useState<boolean>(true)
+	const [isFetching, setIsFetching] = useState<boolean>(true);
 
 	useEffect(() => {
 		const getRate = async () => {
 			try {
 				// const data = await axios.get(`${GET_RATE_URL}/ARBONIASHOP`)
 				// if (data.status !== 200 || data.data.rate <= 0) throw new Error()
-				setRateEuro(DEFAULT_CURRENCY_RATE * DISCOUNT)
-				setIsFetching(false)
+				setRateEuro(DEFAULT_CURRENCY_RATE * DISCOUNT);
+				setIsFetching(false);
 			} catch (err) {
-				setRateEuro(DISCOUNTED_DEF_PRICE)
+				setRateEuro(DISCOUNTED_DEF_PRICE);
 			}
-		}
-		getRate()
-	}, [])
+		};
+		getRate();
+	}, []);
 
-	return { rateEuro, isFetching }
-}
+	return { rateEuro, isFetching };
+};
 
-export default useGetCurrency
+export default useGetCurrency;
